abe7a1995ba00a2d831c15321f3c24f7c9a4cbd6
AshenOne17 <artem08.mail.com@gmail.com>
2023-02-25 21:06:04 -0500

    Additional code cleanup and builds fixes.

--- advanced_calculator/CalculationTypes.cpp
+++ advanced_calculator/CalculationTypes.cpp
@@ -81,80 +81,118 @@ void arithmetic_calculation()
 // *** Explicitly declaring template functions due to file management restrictions ***
 
 template <typename Type>
-void trig_output(const int function_choice, const long double angle, trigonometry_calc<Type>& calc)
+Type apply_tolerance(Type result, Type tolerance)
 {
-	switch (function_choice)
+	if (std::abs(result) < tolerance) 
 	{
-	case 1:
-	{
-		std::cout << "Sin(" << angle << ") = " << calc.sin_calc() << "\n\n";
-		break;
+		return 0.0;
 	}
-	case 2:
+	else 
 	{
-		std::cout << "Cos(" << angle << ") = " << calc.cos_calc() << "\n\n";
-		break;
-	}
-	case 3:
-	{
-		std::cout << "Tan(" << angle << ") = " << calc.tan_calc() << "\n\n";
-		break;
-	}
-	case 4:
-	{
-		std::cout << "Arcsin(" << angle << ") = " << calc.arcsin_calc() << "\n\n";
-		break;
-	}
-	case 5:
-	{
-		std::cout << "Arccos(" << angle << ") = " << calc.arccos_calc() << "\n\n";
-		break;
-	}
-	case 6:
-	{
-		std::cout << "Arctan(" << angle << ") = " << calc.arctan_calc() << "\n\n";
-		break;
-	}
-	default: std::cout << "Input invalidation!\n\n";
+		return result;
 	}
 }
 
 template <typename Type>
-void hyperbolic_output(const int function_choice, const long double angle, trigonometry_hyperbolic_calc<Type>& calc)
+void trig_output(const int function_choice, const long double angle, trigonometry_calc<Type>& calc, Type tolerance)
 {
+	Type result;
+    
+    switch (function_choice)
+    {
+        case 1:
+		{
+			result = calc.sin_calc();
+			std::cout << "Sin(" << angle << ") = " << apply_tolerance(result, tolerance) << "\n\n";
+			break;
+		}
+        case 2:
+		{
+			result = calc.cos_calc();
+			std::cout << "Cos(" << angle << ") = " << apply_tolerance(result, tolerance) << "\n\n";
+			break;
+		}
+        case 3:
+		{
+			result = calc.tan_calc();
+			std::cout << "Tan(" << angle << ") = " << apply_tolerance(result, tolerance) << "\n\n";
+			break;
+		}
+        case 4:
+		{
+			result = calc.arcsin_calc();
+			std::cout << "Arcsin(" << angle << ") = " << apply_tolerance(result, tolerance) << "\n\n";
+			break;
+		}
+        case 5:
+		{
+			result = calc.arccos_calc();
+			std::cout << "Arccos(" << angle << ") = " << apply_tolerance(result, tolerance) << "\n\n";
+			break;
+		}
+        case 6:
+		{
+			result = calc.arctan_calc();
+			std::cout << "Arctan(" << angle << ") = " << apply_tolerance(result, tolerance) << "\n\n";
+			break;
+		}    
+        default:
+		{
+			std::cout << "Input invalidation!\n\n";
+			break;
+		}
+    }
+}
+
+template <typename Type>
+void hyperbolic_output(const int function_choice, const long double angle, trigonometry_hyperbolic_calc<Type>& calc, Type tolerance)
+{
+	Type result;
+
 	switch (function_choice)
 	{
-	case 1:
-	{
-		std::cout << "Sinh(" << angle << ") = " << calc.sinh_calc() << "\n\n";
-		break;
-	}
-	case 2:
-	{
-		std::cout << "Cosh(" << angle << ") = " << calc.cosh_calc() << "\n\n";
-		break;
-	}
-	case 3:
-	{
-		std::cout << "Tanh(" << angle << ") = " << calc.tanh_calc() << "\n\n";
-		break;
-	}
-	case 4:
-	{
-		std::cout << "Arcsinh(" << angle << ") = " << calc.arcsinh_calc() << "\n\n";
-		break;
-	}
-	case 5:
-	{
-		std::cout << "Arccosh(" << angle << ") = " << calc.arccosh_calc() << "\n\n";
-		break;
-	}
-	case 6:
-	{
-		std::cout << "Arctanh(" << angle << ") = " << calc.arctanh_calc() << "\n\n";
-		break;
-	}
-	default: std::cout << "Input invalidation!\n\n";
+		case 1:
+		{	
+			result = calc.sinh_calc();
+			std::cout << "Sinh(" << angle << ") = " << apply_tolerance(result, tolerance) << "\n\n";
+			break;
+		}
+		case 2:
+		{
+			result = calc.cosh_calc();
+			std::cout << "Cosh(" << angle << ") = " << apply_tolerance(result, tolerance) << "\n\n";
+			break;
+		}
+		case 3:
+		{
+			result = calc.tanh_calc();
+			std::cout << "Tanh(" << angle << ") = " << apply_tolerance(result, tolerance) << "\n\n";
+			break;
+		}
+		case 4:
+		{
+			result = calc.arcsinh_calc();
+			std::cout << "Arcsinh(" << angle << ") = " << apply_tolerance(result, tolerance) << "\n\n";
+			break;
+		}
+		case 5:
+		{
+			result = calc.arccosh_calc();
+			std::cout << "Arccosh(" << angle << ") = " << apply_tolerance(result, tolerance) << "\n\n";
+			break;
+		}
+		case 6:
+		{
+			result = calc.arctanh_calc();
+			std::cout << "Arctanh(" << angle << ") = " << apply_tolerance(result, tolerance) << "\n\n";
+			break;
+		}
+		default:
+		{
+			std::cout << "Input invalidation!\n\n";
+			break;
+		} 
+			
 	}
 }
 
@@ -185,6 +223,8 @@ void trigonometry_calculation()
 		angle_value += 2 * M_PIl;
 	}
 
+	constexpr long double tolerance = 1e-10;
+
 	// Trigonometry types of functions
 	if (function_choice == 1)
 	{
@@ -196,7 +236,7 @@ void trigonometry_calculation()
 		const int trigonometric_choice = trig_function_type();
 
 		std::cout << "--------\nOutput result: ";
-		trig_output(trigonometric_choice, angle_value, calc);
+		trig_output(trigonometric_choice, angle_value, calc, tolerance);
 	}
 	// Hyperbolic types of functions
 	else  
@@ -210,7 +250,7 @@ void trigonometry_calculation()
 		const int hyperbolic_choice = hyperbolic_function_type();
 
 		std::cout << "--------\n" << "Output result: ";
-		hyperbolic_output(hyperbolic_choice, angle_value, trig_hyper_calc);
+		hyperbolic_output(hyperbolic_choice, angle_value, trig_hyper_calc, tolerance);
 	}
 }
 
